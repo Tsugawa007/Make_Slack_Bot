42e262472a6cd38aa92f628ddd71cb0682dd7b41
Tsugawa007 <59519629+Tsugawa007@users.noreply.github.com>
2022-10-05 12:57:27 +0900

    Create lambda_function_latest.py

---
+++ lambda_function_latest.py
@@ -0,0 +1,294 @@
+import json
+import logging
+import os
+import urllib.request
+import ssl
+import certifi
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
+#the bottom modules by using gcp api
+import proto
+import datetime
+from google.cloud import vision
+import subprocess
+
+# Instantiates a client
+client_vision_api = vision.ImageAnnotatorClient()
+
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.INFO)
+ssl_context = ssl.create_default_context(cafile=certifi.where())
+client = WebClient(token=os.environ.get('OAuthToken'), ssl=ssl_context)
+
+def judge_back_symbol(symbol):
+    flag = False
+    flag_3 = False
+    if "class" in symbol:
+        flag = True
+        symbol = "class"
+    if "def" in symbol:
+        flag = True
+        symbol = "def"
+    if "for" in symbol:
+        flag = True
+        symbol = "for"
+    if "if" in symbol:
+        flag = True
+        symbol = "if"
+    if "elif" in symbol:
+        flag = True
+        symbol = "elif"
+    if "else" in symbol:
+        flag = True
+        symbol = "else"
+    #0830
+    if "#101" in symbol:
+        flag = True
+        flag_3 = True
+        symbol = "#101"    
+    return flag,symbol,flag_3
+
+def judge_front_symbol(symbol):
+    if ':' in symbol: return True
+    else: return False
+
+def judge_indent(symbol,symbols,symbol_nums):
+    if symbol == "elif" and symbols[-2] == "if":
+        return  symbol_nums[-1]
+    if symbol == "else" and symbols[-2] == "if":
+        return  symbol_nums[-1]
+    if symbol == "else" and symbols[-2] == "elif":
+        return  symbol_nums[-1]
+    if symbol == "if" and symbols[-2] == "else":
+        return symbol_nums[-1] -1
+    if symbol == "def":
+        if len(symbol_nums) > symbols.index("def"):
+            #print(symbol,symbols)
+            return symbol_nums[symbols.index("def")]
+
+    return -1
+
+def content_process(datalist,new_content = ""):
+    indent_cnt = 0
+    symbols = []
+    symbol_nums = []
+    front_flag = False
+    flag_2 = False
+    flag_3 = False
+    dell_ind = ""
+    flag,symbol,flag_3 = judge_back_symbol(datalist[:5])
+    if flag:
+        symbols.append(symbol)
+        symbol_nums.append(1)
+        #indent_cnt = 1
+    for i in range(len(datalist)):
+        if "print" in datalist[i+1:i+7] and datalist[i+6] == " ":
+            dell_ind = i+6
+        
+        if i != dell_ind:
+            new_content += datalist[i]    
+        
+        #new_content += datalist[i]
+        if datalist[i] == '\n':
+            former_cnt = indent_cnt
+            
+            front_symbol = datalist[i-6:i]
+            back_symbol = datalist[i+1:i+5]
+
+            if flag_3:
+                flag = False
+                flag_2  = False
+            else:
+                flag,symbol,flag_3 = judge_back_symbol(back_symbol)
+
+            #print(flag,flag_3,back_symbol)
+            if flag :
+                symbols.append(symbol)
+
+                tmp_cnt= judge_indent(symbol,symbols,symbol_nums)
+                if tmp_cnt != -1:
+                    indent_cnt = tmp_cnt
+                else:
+                    if len(symbol_nums) == 0:
+                        indent_cnt = 0
+                    else:
+                        indent_cnt += 1
+                symbol_nums.append(indent_cnt)
+            else:
+                if judge_front_symbol(front_symbol):
+                    flag_2 = True
+                if flag_2:
+                    new_content += "    "
+            if flag_3:
+                indent_cnt =  0
+            new_content += "    " * indent_cnt
+    return new_content
+
+def load_content(content):
+    image = vision.Image(content=content)
+    # Performs label detection on the image file
+    response =  client_vision_api.document_text_detection(
+        image=image,
+        image_context={'language_hints': ['ja']}
+    )
+    response_content = proto.Message.to_json(response)
+    LOGGER.info(f"Vision_API:{response_content}")
+    text_json = json.loads(response_content)
+    datalist = text_json["textAnnotations"][0]["description"]
+    new_content = content_process(datalist)
+
+    t_delta = datetime.timedelta(hours=9)
+    JST = datetime.timezone(t_delta, 'JST')
+    now = datetime.datetime.now(JST)
+    # Format: YYYYMMDDhhmmss
+    now_time = now.strftime('%Y%m%d%H%M%S')
+    create_file_name = "test" + str(now_time) + ".py"
+    '''
+    with open('/tmp/'+create_file_name, 'wb') as new_content:
+            content = image_file.read()
+    '''
+    LOGGER.info(f"New Content:{new_content}")
+    f = open('/tmp/'+ create_file_name, 'w')
+    f.writelines(new_content)
+    f.close()
+    LOGGER.info("You created a code file from the screenshot of the code successfully!!")
+    return create_file_name
+    
+
+def respond(res="Hello"):
+    #Post the content 
+    ret = {
+        'statusCode': '200',
+        'body': json.dumps(res, ensure_ascii=False),
+        'headers': {
+            #'Content-Type': 'application/json',
+        },
+    }
+    LOGGER.info(f"Return: {ret}")
+    return ret
+
+
+def public_image(file_id):
+    try:
+        #Publicize the image to the channel using slack SDK
+        result = client.files_sharedPublicURL(
+            token=os.environ.get('UserToken'),
+            file=file_id,
+        )
+        # Log the result
+        LOGGER.info(f"Result public image: {result}")
+
+    except SlackApiError as e:
+        LOGGER.info(f"Error public file: {e}")
+
+def post_image_slack(channel,url=None,message=None,flag=False):
+    #Ignore the ssl authentication
+    ctx = ssl.create_default_context()
+    ctx.check_hostname = False
+    ctx.verify_mode = ssl.CERT_NONE
+    
+    #Output the full url
+    LOGGER.info(f"Full url: {url}")
+    
+    #Get the content of the image from the given url
+    with urllib.request.urlopen(url,context=ctx) as web_file, open('/tmp/test.jpg', 'wb') as local_file:
+        local_file.write(web_file.read())
+    
+    if flag:
+        # Loads the image into memory
+        with open('/tmp/test.jpg', 'rb') as image_file:
+            content = image_file.read()
+            LOGGER.info(f"The content using gcp api: {content}")
+        file_path="/tmp/"+load_content(content)
+        file_type="python"
+        LOGGER.info(f"Sceenshot file path: {file_path}")
+        
+        
+    else:
+        file_path="/tmp/test.jpg"
+        file_type="jpg"
+        
+    
+        
+    try:
+        #Upload the image to the channel using slack SDK
+        result = client.files_upload(
+           channels=channel,
+           initial_comment=message,
+           file=file_path,
+           filetype=file_type,
+        )
+        
+        #Output the success log
+        LOGGER.info(f"Result post slack: {result}")
+    except SlackApiError as e:
+        #Output the false log
+        LOGGER.info(f"Error uploading file: {e}")
+       
+def make_image_url(team_id,file_id,file_name,pre_url):
+    #Shape the url
+    ind = pre_url.rfind('-')
+    pub_secret = pre_url[ind+1:]
+    url = f"https://files.slack.com/files-pri/{team_id}-{file_id}/{file_name}?pub_secret={pub_secret}"
+    return url
+    
+def lambda_handler(event, context):
+    print(subprocess.check_output(["cat", "/etc/system-release"]))
+    #202/09/12
+    flag=False
+
+    #Output received the event
+    LOGGER.info(f"Received event: {json.dumps(event)}")
+
+    body = {}
+    #Verify the challeng authentication
+    if event.get('challenge'):
+        return event.get('challenge')
+        
+    #Get the body of the event
+    if event.get('body'):
+        body = json.loads(event['body'])
+        
+    #Check slack signature
+    if event.get('headers', {}).get('X-Slack-Signature'):
+        LOGGER.info(f"Passed X-Slack-Signature!: {json.dumps(event)}")
+
+        try:
+            channel = body['event']['channel']
+            LOGGER.info(f"channel!: {channel}")
+            
+            #image_url
+            team_id = body['team_id']
+            file_id = body['event']['files'][0]['id']
+            file_name =  body['event']['files'][0]['name']
+            pre_url = body['event']['files'][0]['permalink_public']
+
+            #Upload from IOSsmartphone
+            if "iOS" in file_name:
+                file_name = os.environ.get('IphoneName')
+                
+            #Make the image URL
+            url = make_image_url(team_id,file_id,file_name,pre_url)
+            
+            #Publicize the url of the image
+            public_image(file_id)
+            
+            #Make the comment of the image if success
+            message = "Make a code file sucessfully from AWS!!"
+            
+            #post_image_slack(channel,url,message)
+            #return respond(body)
+            
+            LOGGER.info(f"team_id: {team_id} file_id: {file_id} file_name: {file_name}")
+            flag=True
+            post_image_slack(channel,url,message,flag)
+            return respond(body)
+        except KeyError as e:
+            message = "You can post only the image file!!"
+            url = os.environ.get('BadURL')
+            post_image_slack(channel,url,message,flag)
+            return respond(body)
+        
+        
+    return respond(body)
